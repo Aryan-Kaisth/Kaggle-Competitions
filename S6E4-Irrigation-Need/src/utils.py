5a6a91462c5abc0dfd21811793dcbd23f8da76b3
Aryan Kaisth <aryankaisthpvt@gmail.com>
2026-04-12 23:46:22 +0530

    minor fixes

--- S6E4-Irrigation-Need/src/utils.py
+++ S6E4-Irrigation-Need/src/utils.py
@@ -2,17 +2,18 @@ import pandas as pd
 import random
 import os
 import numpy as np
-from sklearn.metrics import balanced_accuracy_score
-import config
-import glob
+import time
 
 def read_csv_file(path: str) -> pd.DataFrame:
+    print(f"Reading file from: {path}")
     return pd.read_csv(path).rename(columns=str.lower)
 
 def save_csv_file(df: pd.DataFrame, path: str) -> None:
+    print(f"Saving file to: {path}")
     df.to_csv(path, index=False)
 
 def seed_everything(seed: int) -> None:
     random.seed(seed)
     np.random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f'Seed set to {seed}')
